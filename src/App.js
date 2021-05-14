091528ea03ebc4d8ab2260179f31ce121cc30339
Iskandarbek <iskandarbekergashev@gmail.com>
2021-05-14 11:31:55 +0500

    last commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import QuizCard from './components/QuizCard';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizCard />
+    </div>
+  );
+}
+
+export default App;
